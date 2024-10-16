c641d72890cdf9013f5fb225ed0e4da03c72280f
Chathura Samarajeewa <chathuras940@gmail.com>
2024-10-17 00:14:06 +0530

    nav bar UI

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,32 @@
+import { Fragment } from "react";
+import { Link, Outlet } from "react-router-dom";
+import { ReactComponent as CrownLogo } from "../../assets/crown.svg";
+import './navigation.styles.css'
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <div>
+            <CrownLogo className="logo" />
+          </div>
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+          <Link className="nav-link" to="/signup">
+            Signup
+          </Link>
+          <Link className="nav-link" to="/login">
+            Login
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navigation;
